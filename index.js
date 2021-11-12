f58ce2642a256bc120eb64278d7247d403292a01
igorpowers <powerfed@mail.ru>
2021-11-12 20:12:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -55,9 +55,7 @@ client.on('messageReactionAdd', (messageReaction, user) => {
 	}
 })
 
-
-
-client.on("message", async message=> {
+client.on("message", async message => {
 
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
