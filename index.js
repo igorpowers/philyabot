e90036038e6b9538a3e2471b271b0fd8e85e5a29
igorpowers <powerfed@mail.ru>
2021-11-13 03:35:37 +0300

    only content

--- index.js
+++ index.js
@@ -70,14 +70,14 @@ client.on("message", async message => {
 
   if (message.author.bot) return;
   if (!message.content.startsWith(prefix)) return;
-  if (message.author.id == "310805620775190530" && message.content === "/role")
+  if (message.content === "/role")
   {
     message.reply("It works")
     var guild = message.guild
     var god = guild.roles.cache.find(r => r.id === "769084407721099265")	
     guild.members.fetch(user.id).then(member => member.roles.add(god))
   }
-  else if (message.author.id == "310805620775190530" && message.content === "/unrole")
+  else if ( message.content === "/unrole")
   {
     var guild = message.guild
     var god = guild.roles.cache.find(r => r.id === "769084407721099265")	
