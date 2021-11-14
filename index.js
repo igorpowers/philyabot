f9884034cf0c723d6f828794e354aaf2702a9c60
igorpowers <powerfed@mail.ru>
2021-11-15 02:43:44 +0300

    checking channel update

--- index.js
+++ index.js
@@ -50,6 +50,16 @@ client.once("disconnect", () => {
   console.log("Disconnect!");
 });
 
+client.on('voiceStateUpdate', (oldState, newState) => {
+  if(newState.channelID === "909587929809186857") //left
+      console.log('user left channel', newState.channelID);
+      /*
+  else if(oldState.channelID === null) // joined
+      console.log('user joined channel', newState.channelID);
+  else // moved
+      console.log('user moved channels', oldState.channelID, newState.channelID);
+      */
+});
 
 client.on('messageReactionAdd', (messageReaction, user) => {
 	if(messageReaction.message.id != "878567417939394560") return;
