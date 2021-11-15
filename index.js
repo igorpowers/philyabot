3f54df75db8e7e65889fda657a0d11ea84384c38
igorpowers <powerfed@mail.ru>
2021-11-16 00:19:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -47,34 +47,29 @@ client.on("ready", () => {
 client.on('voiceStateUpdate', async (oldState, newState) => {
   //const user = await client.users.fetch(newState.id)
   //const member  = newState.guild.member(user)
-  const channelid = '909587929809186857'
+  const swfchannel = '909587929809186857'
   const categoryid = '733312455589101681'
+  const kyfchannel = '909912840339591258  '
 
-  if (newState.channel?.id == channelid){
-    newState.guild.channels.create(`${newState.member.user.username}`, {
+  if (newState.channel?.id == swfchannel){
+    newState.guild.channels.create(`${newState.member.user.username}'s SWF channel`, {
       type: "voice",
       parent: categoryid,
       userLimit: 4,
     }).then(channel=>{
       newState.setChannel(channel)
     })
-  } else if(oldState.channel?.id != channelid && oldState.channel?.parent?.id==categoryid && !oldState.channel?.members.size && oldState.channel?.id != "909827760174809128") oldState.channel.delete()
-/*
-  if (!oldState.channel && newState.channel.id ==='909587929809186857')
-  {
-    const channel = await newState.guild.channels.create(user.username, {
+  } else if(oldState.channel?.id != swfchannel && oldState.channel?.parent?.id==categoryid && !oldState.channel?.members.size && oldState.channel?.id != "909827760174809128") oldState.channel.delete()
+  if (newState.channel?.id == kyfchannel){
+    newState.guild.channels.create(`${newState.member.user.username}'s KYF channel`, {
       type: "voice",
-      parent: newState.channel.parent,
+      parent: categoryid,
+      userLimit: 4,
+    }).then(channel=>{
+      newState.setChannel(channel)
     })
-    member.voice.setChannel(channel)
-    voiceCollection.set(user.id, channel.id)
-  }
-  else if(!newState.channel)
-  {
-    if(oldState.channelID === voiceCollection.get(newState.id)) 
-      return oldState.channel.delete()
-  }
-    */
+  } else if(oldState.channel?.id != kyfchannel && oldState.channel?.parent?.id==categoryid && !oldState.channel?.members.size && oldState.channel?.id != "909827760174809128") oldState.channel.delete()
+
 })
 
 client.on('messageReactionAdd', (messageReaction, user) => {
