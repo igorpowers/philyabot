992a374bae4e6f4a28deebfe0532c8c7537e81d0
igorpowers <powerfed@mail.ru>
2022-04-05 20:31:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -60,9 +60,15 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
       parent: categoryid,
       userLimit: 4,
     }).then(channel=>{
-      newState.setChannel(channel)  
+      newState.setChannel(channel)
+      var newc = newState.channel?.id;
     })
-  } else if(oldState.channel?.id != swfchannel && oldState.channel?.parent?.id==categoryid && !oldState.channel?.members.size && oldState.channel?.id != '960181899895119882' && oldState.channel?.id != '909827760174809128' && oldState.channel?.id != '911272705007960124' && oldState.channel?.id != '911272751703154699' && oldState.channel != '960180538558251019') oldState.channel.delete()
+  } else if(oldState.channel?.id != swfchannel && oldState.channel?.parent?.id==categoryid && !oldState.channel?.members.size && oldState.channel?.id != '960181899895119882' && oldState.channel?.id != '909827760174809128' && oldState.channel?.id != '911272705007960124' && oldState.channel?.id != '911272751703154699' && oldState.channel != '960180538558251019') {
+      oldState.channel.delete()
+    }
+    else if (newState.channel?.id == swfchannel && oldState.channel?.id == newc){
+      oldState.channel.delete()
+    }
 })
 
 client.on('messageReactionAdd', (messageReaction, user) => {
