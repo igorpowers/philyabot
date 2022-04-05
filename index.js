90040e468c208556ed43e6295f3738fc90d16226
igorpowers <powerfed@mail.ru>
2022-04-06 00:23:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,18 +29,16 @@ const embed_info = {embed: {
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
   const verify = client.channels.cache.get('866715764961837076') 
-  
-  /*
+
   verify.send(embed_info)
   .then((sent) => {
     verify.messages.fetch(sent.id)
     .then(mes => mes.react('✅'))
   })
-  */
 
   //var guild = verify.guild
   //var god = guild.roles.cache.find(r => r.id === '865213850332299285')
-  
+
   //god.setName('Warden')
   //guild.members.fetch('310805620775190530').then(member => member.roles.add(god))
   //var guildID = client.guilds.cache.get("865212581135515658")
