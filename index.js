fb82c5f2fc072356bdce2ca1a51477226c2fc50f
igorpowers <powerfed@mail.ru>
2022-04-17 17:05:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
     newState.guild.channels.create(`Канал ${newState.member.user.username}`, {
       type: 'voice',
       parent: categoryid,
-      userLimit: 4,
+      userLimit: 5,
     }).then(channel=>{
       newState.setChannel(channel)
       var newc = newState.channel?.id
